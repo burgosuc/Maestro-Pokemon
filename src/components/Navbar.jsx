f1865162045e793b5d80a52655928b16e34c3b4a
burgosuc <jlb1983@gmail.com>
2023-11-13 20:04:31 -0300

    corregir error en Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,18 @@
 import Navbar from 'react-bootstrap/Navbar'
 import { NavLink } from 'react-router-dom'
-import pikachuImg from '../assets/imgs/Location_icon.png' // Importa la imagen correctamente
+import pikachuImg from '../assets/imgs/Location_icon.png'
 import '../App.css'
 
 const PokeNavbar = () => {
+  const setActive = ({ isActive }) => (isActive ? 'm-3 active' : 'm-3')
   return (
-    <Navbar className='navBar'>
+    <Navbar className='d-flex justify-content-between px-5' variant='light' style={{ background: '#58585A' }}>
       <Navbar.Brand>
-        <img src={pikachuImg} width='50px' alt='Pikachu' />
+        <img width='50' src={pikachuImg} alt='imagen pokemon' />
       </Navbar.Brand>
-
-      <div className='d-flex links'>
-        <NavLink className='nav-link' activeClassName='active' to='/home'>
-          Home
-        </NavLink>
-        {' - '}
-        <NavLink className='nav-link' activeClassName='active' to='/Pokemon'>
-          Pokemones
-        </NavLink>
+      <div>
+        <NavLink className={setActive} to='/'>Home</NavLink>
+        <NavLink className={setActive} to='/pokemon'>Pokemones</NavLink>
       </div>
     </Navbar>
   )
